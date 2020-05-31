502b2564eb05bca3a9021411cc59f0b2c962082d
vincentx_3 <2440027575@qq.com>
2020-05-31 09:34:48 +0800

    add raspberry pi video stream sh

---
+++ raspberry_stream/startup.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 30 -r 1280x720" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8080 -w /usr/local/share/mjpg-streamer/www"
+
